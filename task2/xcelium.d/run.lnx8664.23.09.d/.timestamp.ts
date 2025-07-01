cbb9d5f503c62d6077edf4d86e7fb27da21b264a
Abdur Rafay <a.rafay9999@gmail.com>
2025-07-01 09:28:50 +0500

    Delete task2/xcelium.d directory

--- task2/xcelium.d/run.lnx8664.23.09.d/.timestamp.ts
+++
@@ -1,7 +0,0 @@
-1731471878 /home/cc/Downloads/Abdur_Rafay_Data/UVM_Labs/lab_42/task2/tb/yapp_router.sv
-1731471878 /home/cc/Downloads/Abdur_Rafay_Data/UVM_Labs/lab_42/task2/tb/clkgen.sv
-1751268598 /home/cc/Downloads/Abdur_Rafay_Data/UVM_Labs/lab_42/task2/tb/hw_top_dut.sv
-1751119710 /home/cc/Downloads/Abdur_Rafay_Data/UVM_Labs/lab_42/task2/tb/tb_top.sv
-1751120208 /home/cc/Downloads/Abdur_Rafay_Data/UVM_Labs/lab_42/task2/sv/yapp_pkg.sv
-1751120395 /home/cc/Downloads/Abdur_Rafay_Data/UVM_Labs/lab_42/task2/sv/yapp_if.sv
-1751267137 /home/cc/Downloads/Abdur_Rafay_Data/UVM_Labs/lab_42/task2/tb/hw_top_no_dut.sv
--- task2/xcelium.d/run.lnx8664.23.09.d/files.ts
+++
@@ -1,6 +0,0 @@
-1751120395 /home/cc/Downloads/Abdur_Rafay_Data/UVM_Labs/lab_42/task2/sv/yapp_if.sv
-1751120208 /home/cc/Downloads/Abdur_Rafay_Data/UVM_Labs/lab_42/task2/sv/yapp_pkg.sv
-1731471878 /home/cc/Downloads/Abdur_Rafay_Data/UVM_Labs/lab_42/task2/tb/clkgen.sv
-1751119710 /home/cc/Downloads/Abdur_Rafay_Data/UVM_Labs/lab_42/task2/tb/tb_top.sv
-1731471878 /home/cc/Downloads/Abdur_Rafay_Data/UVM_Labs/lab_42/task2/tb/yapp_router.sv
-1751268598 /home/cc/Downloads/Abdur_Rafay_Data/UVM_Labs/lab_42/task2/tb/hw_top_dut.sv
